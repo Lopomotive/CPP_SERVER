8ce6e21252a6ebc51686deb6fbab9f8d074583a8
Lopomotive <mikehawk.iblm@gmail.com>
2024-05-28 22:24:52 +0200

    cpp_dump install

--- cpp_dump.sh
+++ cpp_dump.sh
@@ -1,10 +1,15 @@
 ###!bin/bash 
 #!bin/zsh
 
+$FOLDERNAME="cpp_dump"
 $CPP_DUMP_INSTALL=$(git clone https://github.com/philip82148/cpp-dump.git) 
-if [-e "$CPP_DUMP_INSTALL"]; then
-  echo "Installing"
+if [! -d "$FOLDERNAME"]; then   
+  if [-e "$CPP_DUMP_INSTALL"]; then
+    echo "Installing $FOLDERNAME"
+  else
+    echo "Failed to install cpp_dump"
+  fi 
 else
-  echo "Failed to install cpp_dump"
-fi 
+  echo "Folder already exists"
+fi
 
