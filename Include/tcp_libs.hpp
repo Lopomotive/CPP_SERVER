7c5e0a2f9c33db768694e73cf7c32bbf853885bf
Lopomotive <mikehawk.iblm@gmail.com>
2024-05-29 22:09:52 +0200

    General libary header file

---
+++ Include/tcp_libs.hpp
@@ -0,0 +1,14 @@
+#include <sys/socket.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h> 
+
+#pragma once  
+//Define warning to get rid off
+//CLang instead of gcc
+#define WARNING 
+#pragma ( push )
+#pragma ( disable : WARNING )
+#pragma ( pop )
