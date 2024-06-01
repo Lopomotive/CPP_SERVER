ed8c62956728392408a818e581355b7a27a68958
Lopomotive <mikehawk.iblm@gmail.com>
2024-06-01 23:43:57 +0200

    Unfinnished

--- Include/server_create.hpp
+++ Include/server_create.hpp
@@ -1,31 +1,34 @@
-#include <sys/socket.h> 
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <string> 
+#include "debugger.hpp"
+#include "server_observer.hpp"
+#include "tcp_libs.hpp"
+#include "null_val.hpp"
+#include <string>
 
-class Name {
-public:
-  Name();
-  Name(Name &&) = default;
-  Name(const Name &) = default;
-  Name &operator=(Name &&) = default;
-  Name &operator=(const Name &) = default;
-  ~Name();
+//If not included
 
-private:
-  
-};
-
-Name::Name() {
+class SocketOptions{
+  public: 
+    //Check directory and look in files that end with .conf
+    //Continue the function below
+    void SocketOptionsConfigFile(){
+      string Socket_config_file = "socketoptions.conf"; 
+    }
+  protected: 
+    char(socket_buffer[]);
+    //If condition may be moved?
+    if(socket_buffer == nullptr){
+      DEBUG(socket_buffer); 
+      perror << socket_buffer;
+      exit(EXITFAILURE);
+    }
+  private: 
+    auto SocketConfig(){
+      constexpr int opt = 1; 
+      socklen_t optlen=sizeof(opt); 
+      int sock=socket(AF_UNIX, SOCK_STREAM, 0);}
+      //Should work with the "null_val.hpp file"
+      ValueCheck(CheckValid(sock));
+      setsockopt(sock, SOL_SOCKET, SO_REUSEADDR|SO_REUSEPORT, &opt, optlen);
+    }
 }
 
-Name::~Name() {
-} 
-
-class Server{
-  public:
-  protected:
-    ip_addr()
-    virtual void SocketConfig(){}
-
-}
